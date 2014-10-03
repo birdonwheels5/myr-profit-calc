aa1f4dffbdec84352d9ae16cf7a3c3729c18ed85
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-10-03 11:24:18 -0400

    Update index.php

--- index.php
+++ index.php
@@ -86,19 +86,14 @@
 		}
 		
 		$sha_diff = number_format($diff[0], 2, '.', ',');
-		$sha_net_hashrate = number_format(($diff[0]/34.92331797)/1000, 2, '.', ',');
 		
 		$scrypt_diff = number_format($diff[1], 2, '.', ',');
-		$scrypt_net_hashrate = number_format($diff[1]/34.92331797, 2, '.', ',');
 		
 		$skein_diff = number_format($diff[2], 2, '.', ',');
-		$skein_net_hashrate = number_format($diff[2]/34.92331797, 2, '.', ',');
 		
 		$groestl_diff = number_format($diff[3], 2, '.', ',');
-		$groestl_net_hashrate = number_format($diff[3]/34.92331797, 2, '.', ',');
 		
 		$qubit_diff = number_format($diff[4], 2, '.', ',');
-		$qubit_net_hashrate = number_format($diff[4]/34.92331797, 2, '.', ',');
 		
 		
 		calculate_profit($sha_hashrate, $scrypt_hashrate, $skein_hashrate, $groestl_hashrate, $qubit_hashrate);
@@ -182,35 +177,6 @@
 			
 		}
 		
-		if($_POST["refresh_values"])
-		{
-
-		$diff = get_avg_diffs($avg);
-		
-		$sha_diff = number_format($diff[0], 2, '.', ',');
-		$sha_net_hashrate = number_format(($diff[0]/34.92331797)/1000, 2, '.', ',');
-		
-		
-		$scrypt_diff = number_format($diff[1], 2, '.', ',');
-		$scrypt_net_hashrate = number_format($diff[1]/34.92331797, 2, '.', ',');
-		
-		
-		$skein_diff = number_format($diff[2], 2, '.', ',');
-		$skein_net_hashrate = number_format($diff[2]/34.92331797, 2, '.', ',');
-		
-		
-		$groestl_diff = number_format($diff[3], 2, '.', ',');
-		$groestl_net_hashrate = number_format($diff[3]/34.92331797, 2, '.', ',');
-		
-		
-		$qubit_diff = number_format($diff[4], 2, '.', ',');
-		$qubit_net_hashrate = number_format($diff[4]/34.92331797, 2, '.', ',');
-		
-		
-		calculate_profit($sha_hashrate, $scrypt_hashrate, $skein_hashrate, $groestl_hashrate, $qubit_hashrate);
-		
-		}
-		
 		if($_POST["clear"])
 		{
 			$sha_hashrate = 1;
@@ -257,19 +223,6 @@ Each algorithm has an independent difficulty, and a block time of 2.5 minutes, w
 The 5 algorithms that comprise Myriadcoin are Sha256, Scrypt, Groestl, Skein, and Qubit. 
 For more information, visit <a href="http://myriadplatform.org" target="_blank">MyriadPlatform.org</a></p>
 
-	<p><h4>Links of interest:</h4> 
-	<ul>
-		<li><a href="http://birdonwheels5.no-ip.org/richlist/"target="_parent">Top 1000 Richest Myriadcoin Addresses</a>
-		<li><a href="http://birdonwheels5.no-ip.org/myriad-bounty/" target="_parent">Myriadcoin Bounty Board</a>
-		<li><a href="http://birdonwheels5.no-ip.org/myr-hub/" target="_parent">Bird's Myriadcoin P2pool Hub</a>
-		<li><a href="http://birdonwheels5.no-ip.org/home/" target="_parent">Bird's Website Home Page</a>
-	</ul></p>
-	
-	<p><h4>Did you know?</h4> You can change the currency that values are displayed in to USD! 
-	Click on MYR in the top right corner of the page, and select USD. (This feature is still in development)
-	<br/>
-	</p>
-
         <p><h3>Algorithm Statistics:</h3> 
         	    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
 		    <input type="submit" name="refresh_values" value="Refresh Values">
@@ -284,20 +237,12 @@ For more information, visit <a href="http://myriadplatform.org" target="_blank">
 		<td><b>Qubit</b></td>
 	    </tr>
 	    <tr>
-		<th>Current Difficulty:</th>
+		<th>Average Difficulty:</th>
 		<td ><?php print $sha_diff; ?></td>
 		<td><?php print $scrypt_diff; ?></td>
 		<td><?php print $skein_diff; ?></td>
 		<td><?php print $groestl_diff; ?></td>
 		<td><?php print $qubit_diff; ?></td>
-	    </tr>
-	    <tr>
-		<th>Network Hashrate:</th>
-		<td><?php print $sha_net_hashrate; ?><br/>TH/S</td>
-		<td><?php print $scrypt_net_hashrate; ?><br/>GH/s</td>
-		<td><?php print $skein_net_hashrate; ?><br/>GH/s</td>
-		<td><?php print $groestl_net_hashrate; ?><br/>GH/s</td>
-		<td><?php print $qubit_net_hashrate; ?><br/>GH/s</td>
 	    </tr>
 	   <tr>
 		<th>Profitability/Day:<br/>
