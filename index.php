c02d98722c50ed6ca6cf89c43674edede74a98e0
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-10-04 11:36:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -424,10 +424,13 @@ For more information, visit <a href="http://myriadplatform.org" target="_blank">
 			<tr/>
 			<tr>
 				<td>
-					
+					&nbsp;
 				</td>
 				<td>
 					<input type="submit" name="submit" value="Submit"><input type="submit" name="clear" value="Clear">
+				</td>
+				<td>
+					&nbsp;
 				</td>
 			</tr>
 		</table></center>
