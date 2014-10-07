81a92ea87bc619264134a84dae7dff817b76f1e0
birdonwheels5 <birdonwheels5@users.noreply.github.com>
2014-10-06 21:08:51 -0400

    Update index.php

--- index.php
+++ index.php
@@ -444,14 +444,13 @@
 			<article>
 	<div class="inputHolder">			
 	<div class="welcome">
-		<center><h3><img src="http://birdspool.no-ip.org:5567/static/img/logo.png" style="width:2%;"</img>  Myriadcoin Profitability Calculator</h3></center>
+		<center><h2><img src="http://birdspool.no-ip.org:5567/static/img/logo.png" style="width:2%;"</img>  Myriadcoin Profitability Calculator</h2></center>
         <p>
 	Welcome! This tool lets you figure out mining profitability for all the MYR algorithms. 
 	You don't need to fill out all the fields. The default difficulty is 
 	calculated from the average of difficulties taken every hour over the past 24 hours, so it is  
 	resistant to difficulty fluctuations. Alternatively, you can choose to use the 
-	 average over a longer period of time for even more resistance. Enjoy!<br/><br/>
-	 
+	 average over a longer period of time for even more resistance. Enjoy!<br/>
 	For more information regarding Myriadcoin, visit <a href="http://myriadplatform.org" target="_blank">MyriadPlatform.org</a>
 	</div>
         	    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
@@ -614,13 +613,10 @@
 		</table>
 		<table class="mainTable">
 			<tr>
-				<td>
-					&nbsp;
-				</td>
-				<td>
+				<center><td>
 					<input type="submit" name="submit" value="Submit">&nbsp;
 					<input type="submit" name="clear" value="Clear">
-				</td>
+				</td></center>
 			</tr>
 			<tr>
 				<td>
